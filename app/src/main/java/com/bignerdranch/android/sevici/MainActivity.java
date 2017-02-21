b02900e1feab93ed61f6d0455e99cce107498bae
jesnavino <jesnavino@alum.us.es>
2017-02-21 16:07:14 +0100

    Google Maps

--- app/src/main/java/com/bignerdranch/android/sevici/MainActivity.java
+++ app/src/main/java/com/bignerdranch/android/sevici/MainActivity.java
@@ -4,12 +4,27 @@ import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
+import android.widget.ImageButton;
 
 public class MainActivity extends AppCompatActivity {
+
+    private ImageButton mImageButton;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        mImageButton = (ImageButton) findViewById(R.id.imageButton3);
+        mImageButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent mainIntent = new Intent().setClass(MainActivity.this, MapsActivity.class);
+                startActivity(mainIntent);
+            }
+        });
+
     }
 }
