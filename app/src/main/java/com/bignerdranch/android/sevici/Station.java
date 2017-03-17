1908f30bed9f0fd3b637da11f9b441670d90258a
jesnavino <jesnavino@alum.us.es>
2017-03-17 18:21:48 +0100

    Subida la clase estacion

---
+++ app/src/main/java/com/bignerdranch/android/sevici/Station.java
@@ -0,0 +1,62 @@
+package com.bignerdranch.android.sevici;
+
+/**
+ * Created by CHUL on 17/03/2017.
+ */
+
+public class Station {
+
+    private String lat;
+    private String lng;
+    private String idEstacion;
+    private String direccion;
+
+    public Station(){
+        super();
+    }
+
+    public Station(String lat, String lng, String idEstacion, String direccion){
+        this.lat=lat;
+        this.lng=lng;
+        this.idEstacion=idEstacion;
+        this.direccion=direccion;
+    }
+
+    public String getLat() {
+        return lat;
+    }
+
+    public void setLat(String lat) {
+        this.lat = lat;
+    }
+
+    public String getLng() {
+        return lng;
+    }
+
+    public void setLng(String lng) {
+        this.lng = lng;
+    }
+
+    public String getIdEstacion() {
+        return idEstacion;
+    }
+
+    public void setIdEstacion(String idEstacion) {
+        this.idEstacion = idEstacion;
+    }
+
+    public String getDireccion() {
+        return direccion;
+    }
+
+    public void setDireccion(String direccion) {
+        this.direccion = direccion;
+    }
+
+    @Override
+    public String toString() {
+        return "Estacion [lat=" + lat + ", lng=" + lng + ", idEstacion=" + idEstacion + ", direccion=" + direccion
+                + "]";
+    }
+}
